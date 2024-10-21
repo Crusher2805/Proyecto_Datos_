9918cd637f7dfad2c8c8a47aaae84c1bcc2b545d
Sebscr <sebascr1604@hotmail.com>
2024-10-20 18:27:22 -0600

    Proyecto_EstructuraDatos.java

--- Proyecto_EstructuraDatos/src/proyecto_estructuradatos/Proyecto_EstructuraDatos.java
+++ Proyecto_EstructuraDatos/src/proyecto_estructuradatos/Proyecto_EstructuraDatos.java
@@ -9,6 +9,20 @@ public class Proyecto_EstructuraDatos {
     public static void main(String[] args) {
        ReservaVuelos vuelos = new ReservaVuelos();
        vuelos.mostrarMenu();
+
+        Cola cola = new Cola();
+        
+        DatoC datoVuelo = new DatoC();
+        datoVuelo.setOrigen("Nueva York");
+        datoVuelo.setDestino("Toronto");
+        datoVuelo.setNumeroVuelo("12-E");
+        datoVuelo.setPrecio(200);
+
+    
+        cola.encolar(datoVuelo);
+
+    
+        cola.imprimirCola();
     }
     
 }
